c509ffd7abfc8351cf2df6b6e52f4d37a7f53b91
shaun <firedragon888@protonmail.com>
2022-02-20 20:03:43 +0000

    origin

--- web/static/script/contact_app.js
+++ web/static/script/contact_app.js
@@ -2,19 +2,39 @@
 APP = {}
 
 APP.remote = function(){
-    let _profiles;
+    // data
+    let _profiles,
+    // gui parts
+        _profile_con = $('#contacts-con');
 
+
+    function redraw_profiles(){
+        let tmpl_str = [
+            '{{#profiles}}',
+                // if we have a name use that
+                '<div>',
+                    '{{#attrs.name}}',
+                        '{{attrs.name}}',
+                    '{{/attrs.name}}',
+                    // else just the pub_k
+                    '{{^attrs.name}}',
+                        '{{pub_k}}',
+                    '{{/attrs.name}}',
+                '</div>',
+            '{{/profiles}}'
+            ].join('');
+
+        _profile_con.html(Mustache.render(tmpl_str, _profiles));
+    }
 
     return {
         'load_profiles' : function(){
             $.ajax({
                 url: '/profiles'
             }).done(function(data) {
-                alert('wtf')
-                console.log(data);
+                _profiles = data;
+                redraw_profiles();
             });
-
-
         }
     }
 }();
